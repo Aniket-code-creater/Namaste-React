d089e3016610f8095073fb863e1cea08e0c23eea
Aniket Rajput <aniketmakwana1010@gmail.com>
2024-12-23 21:47:43 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+  "div",
+  { id: "parents" },
+  React.createElement("div", { id: "parent" }, [
+    React.createElement("h1", { id: "child1" }, "Hello i,m child"),
+    React.createElement("h2", { id: "child2" }, "Hello i,m child"),
+  ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
